1fcfba0917ed743248b9d549132aad72acf26e4f
David Lee <leed@leed.ch>
2017-07-09 23:22:55 +0200

    Added Tests

---
+++ tests/Core/Html5Test.php
@@ -0,0 +1,111 @@
+<?php
+
+namespace Leedch\Html\Core;
+
+use Leedch\Html\Core\Html5;
+use PHPUnit\Framework\TestCase;
+
+class Html5Test extends TestCase {
+
+    public function testDocType() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testHtmlDocument() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testBr() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testHtml() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTitle() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testHead() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testBody() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testForm() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testButton() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testLabel() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testInput() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTd() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTh() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTr() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTbody() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testThead() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTable() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testRenderTable() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testDiv() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+    public function testTag() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+
+}
---
+++ tests/Core/HtmlTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Leedch\Html\Core;
+
+use Leedch\Html\Core\Html;
+use PHPUnit\Framework\TestCase;
+
+class HtmlTest extends TestCase {
+
+    public function testTag() 
+    {
+        $this->assertEquals('your input', 'your input');
+    }
+
+
+}
